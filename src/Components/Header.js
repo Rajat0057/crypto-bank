54e0f60702ff4ec9deb8b05bd5c3266d85543b64
amit.verma <amit.verma@extramarks.com>
2022-07-26 15:32:41 +0530

    crypto bank

---
+++ src/Components/Header.js
@@ -0,0 +1,74 @@
+import React from "react";
+// import { useStyles } from "@material-ui/core";
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  makeStyles,
+  Typography,
+  Select,
+  MenuItem,
+  createTheme,
+  ThemeProvider,
+} from "@material-ui/core";
+import { useNavigate } from "react-router-dom";
+import { CryptoState } from "../CryptoContext";
+
+const useStyles = makeStyles(() => ({
+  title: {
+    flex: 1,
+    color: "gold",
+    fontFamily: "Montserrat",
+    fontWeight: "bold",
+    cursor: "pointer",
+  },
+}));
+function Header() {
+  const classes = useStyles();
+
+  const navigate = useNavigate();
+  const { currency, setCurrency } = CryptoState();
+
+  console.log(currency);
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              onClick={() => navigate("/")}
+              variant="h6"
+              className={classes.title}
+            >
+              Crypto Bank
+            </Typography>
+            <Select
+              variant="outlined"
+              style={{
+                width: 100,
+                height: 40,
+                marginRight: 15,
+              }}
+              value={currency}
+              onChange={(e) => setCurrency(e.target.value)}
+            >
+              <MenuItem value={"USD"}>USD</MenuItem>
+              <MenuItem value={"INR"}>INR</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
+}
+
+export default Header;
